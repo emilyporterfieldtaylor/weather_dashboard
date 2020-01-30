6a1aa7883fe395e4460e0f72da033f2e878ed848
Emily Taylor <emilyporterfieldtaylor@gmail.com>
2020-01-29 22:46:46 -0600

    create main weather ajax calls and add moment js to format unix date

--- script.js
+++ script.js
@@ -1,32 +1,59 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
-});
+var cityList = [];
+const apiKey = "ef8cf9f7ace571698b37ad03714d34f7";
 
-var city = "Austin";
-// var city = JSON.parse(localStorage.getItem("city"));
-var apiKey = "ef8cf9f7ace571698b37ad03714d34f7";
-var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + apiKey;
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response){
-    console.log(response);
-})
-
-if (city != null){
-    $("input").each(function (index, element){
-        var currentCitySavedValue = city[index];
-        $(element).val(currentCitySavedValue); 
+function GetWeather(city) {
+    var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + apiKey;
+    $.ajax({
+        url: weatherURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+         var dateTime = (" " + moment(response.dt, "X").format("MM/DD/YYYY"));
+        $("#cityMain").text(response.name + " " + dateTime);
+        $("#temperature").text(Math.floor((response.main.temp - 273.15) * 1.80) + 32);
+        //$("#temperature").text(response.main.temp + " " + response.weather[0].icon);
+        $("#humidity").text(response.main.humidity);
+        $("#windSpeed").text(response.wind.speed);
     });
 }
 
-$("button").click(function(){
-    $(".hide").removeClass("hide");
-    city = $("input").map(function(){
-        return $(this).val();
-    });
-    localStorage.setItem("city", JSON.stringify(city));
-    $(".searchHistory").prepend("<button type=\"button\" class=\"btn btn-outline-info\">" + city[0] + "</button>");
-   });
+function addButton(city) {
+    $(".searchHistory").prepend("<div class='row'><div class='col'><button type=\"button\" class=\"btn btn-outline-info city-btn\">" + city + "</button></div></div>");
+}
 
+function searchCity(city) {
+    if (city === null || city === '') {
+        return;
+    }
+    $(".hide").removeClass("hide");
+    GetWeather(city);
+}
+
+$(document).ready(function () {
+    cityList = JSON.parse(localStorage.getItem("cityList"));;
+    
+    if (cityList === null)
+    {
+        cityList = [];
+    }
+
+    $(cityList).each(function(index, element){
+        addButton(element);
+    });
+
+    $(".city-btn").click(function () {
+        var cityName = $(this).text();
+        searchCity(cityName);
+    });
+
+    $("#citySearch").click(function () {
+        var city = $("input").val();
+        searchCity(city);
+        if ($.inArray(city, cityList) > -1){
+            return;
+        }
+        addButton(city);
+        cityList.push(city);
+        localStorage.setItem("cityList", JSON.stringify(cityList));
+    });
+});
